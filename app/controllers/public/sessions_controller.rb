07dfeeb362c0fc977984a25420d56fffec8b8c07
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-13 00:53:07 +0000

    [Fix]new registration is now available

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,10 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number, :password])
+  end
 end
