578c5658815082c796aa34a408f3fd9256bda869
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-14 14:10:32 +0000

    [Add]it is now possible to display the last name when logging in as a customer, and routing has been added to my page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ devise_for :customers,skip: [:passwords], controllers: {
   root to: 'public/homes#top'
   get "about" => "public/homes#about", as: "about"
   get "customers/my_page"=> "public/customers#show"
+  get "customers/information/edit" => "public/customers#edit"
+  patch "customers/information" => "public/customers#update"
 # 管理者用
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
