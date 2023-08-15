7d161b51513ea10f3066714762d5187e25783d79
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-15 13:26:50 +0000

    [Add}I tried to write a withdrawal function in the Public SessionsController

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -31,7 +31,10 @@ class Public::SessionsController < Devise::SessionsController
     def customer_state
       @customer = Customer.find_by(email: params[:customer][:email])
       return if !@customer
-      if @customer.valid_password?(params[:customer][:password])
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+        redirect_to new_customer_registration
+      else
+        render :crea
       end
     end
 
