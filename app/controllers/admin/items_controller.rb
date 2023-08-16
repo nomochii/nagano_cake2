1310a722879f1174b55d9b108434ac619610316b
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-16 07:37:53 +0000

    [Add]Creating a new product registration screen for admin

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,10 +5,10 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @admin_item = Item.new(item_params)
-    @admin_item.customer = current_customer
-    @admin_item.save
-    redirect_to items_path(@admin_item.id)
+    @item = Item.new(item_params)
+    @item.customer = current_customer
+    @item.save
+    redirect_to items_path(@item.id)
   end
 
   def index
@@ -21,6 +21,21 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+  
+  def update
+     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
