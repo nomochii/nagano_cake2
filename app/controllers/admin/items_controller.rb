1ef19180bded433f9d01840e4c9e48755533f784
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-16 09:17:35 +0000

    [Defect]Cannot register new product

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,15 +20,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.all
     @item = Item.find(params[:id])
   end
-  
+
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
     redirect_to items_path
   end
-  
+
   def update
      @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -36,6 +37,14 @@ class Admin::ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
   end
 
 
