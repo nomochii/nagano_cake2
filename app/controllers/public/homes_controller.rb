6431c43417ddd5c45b65aee6b1e8f798a65bef84
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-17 16:52:07 +0000

    [Add]New products can now be displayed on the customer My Page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,13 @@
 class Public::HomesController < ApplicationController
   def top
-
+    @items = Item.all
   end
 
   def about
 
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :price)
   end
 end
