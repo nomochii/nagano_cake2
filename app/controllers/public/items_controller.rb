eec990a046df0cab642371d7a9ae049f0e7b9db3
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-18 05:11:28 +0000

    [Add]Customer's product list creation

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,12 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+     @items = Item.all
+    # @items = Item.count
+  end
+
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :price)
+  end
 end
