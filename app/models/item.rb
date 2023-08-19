05e3aad0610a3f165b2e979d3f64b584b7f0f57a
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-19 16:09:23 +0000

    [Add]I tried to create an association other than admin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
 
    has_one_attached :image
-   # belongs_to :admin
+   has_many :order_details, dependent: :destroy
+   has_many :cart_items, dependent: :destroy
 
    def  add_tax_price
       (self.price * 1.10).round
