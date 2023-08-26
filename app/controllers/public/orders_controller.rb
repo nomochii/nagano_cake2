c1b38930967479627c00adcc26a0b79c330cf1a0
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-26 12:50:11 +0000

    [Add]注文確認画面と機能があらかたできた。

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,37 +1,27 @@
 class Public::OrdersController < ApplicationController
 
-   # 購入情報の入力画面で、宛先や住所などを入力する所
+   # 購入情報の入力画面で、宛先や住所などを入力する所（フォームを作成するのに使う）
   def new
     @order = Order.new
   end
 
-  # 購入を確定する
-  def create    # Order に情報を保存する
-    # ログインユーザーのカートアイテムを全て取り出して cart_items に入れる
+
+  def create
+    order = Order.new(order_params)
+    order.customer_id = current_customer.id
+    order.save
     @cart_items = current_customer.cart_items.all
-    # 渡ってきた値を @order に入れる
-    @order = current_customer.orders.new(order_params)
-    # ここに至るまでの間にチェックは済ませているが、念の為IF文で分岐させてる
-    if @order.save
-      cart_items.each do |cart|  # 取り出したカートアイテムの数繰り返し
-      # order_item にも一緒にデータを保存する必要があるのでここで保存
-        order_confirm = OrderConfirm.new
-        order_confirm.customer_id = cart_item.current_customer
-        order_confirm.order_id = @order.id
-        order_confirm.order_amount = cart_item.amount
-        # 購入が完了したらカート情報は削除するのでこちらに保存
-        order_confirm.order_price = cart_item.item.price
-        # カート情報を削除するので item との紐付けが切れる前に保存
-        order_item.save
-      end
-      # 遷移したいページのパス
-      redirect_to orders_complete_path
-      # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除(カートを空にする)
-      cart_items.destroy_all
-    else
-       @order = Order.new(order_params)
-       render :new
+
+    @cart_items.each do |cart_item|
+      @order = OrderDetail.new
+      @order.order_id = order.id
+      @order.item_id = cart_item.item.id
+      @order.amount = cart_item.amount
+      @order.purchase_price = cart_item.item.price * 1.1
+      @order.save!
     end
+        @cart_items.destroy_all
+        redirect_to orders_complete_path
   end
 
   def index
@@ -42,26 +32,38 @@ class Public::OrdersController < ApplicationController
   def confirm
     # new 画面から渡ってきたデータを @order に入れる
     @order = Order.new(order_params)
-    # @order の各カラムに必要なものを入れる
-    # 模索中
-    # @order.name = current_customer.name
-    # @order.customer_id = current_customer.id
-    # @orders = current_customer.cart_items.all
-    # @orders = current_customer.orders.all
-    # @order.save
-    redirect_to orders_complete_path
+      # @cart_items.each do |cart_item|
+      #   @cart_item.customer_id = current_customer.id
+      #   @cart_items = current_customer.cart_items.all
+      #   @order = OrderDetail.new
+      #   @order.order_id = order.id
+      #   @order.item_id = cart_item.item.id
+      #   @order.price = cart_item.item.price
+      #   @order.amount = cart_item.amount
+      #   @order.save!
+      # end
+
+
+    @order.shipping_postal_code = current_customer.postal_code
+    @order.shipping_address = current_customer.address
+    @order.shipping_name = current_customer.first_name + current_customer.last_name
+    @order.postage = 800
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order.total_amount = @total + @order.postage
+
   end
 
-  # def complete
-  # end
+  def complete
+  end
 
-  # def show
-  #   @order = Order.find(params[:id])
-  #   @ordere_details = @order.ordere_details
-  # end
+  def show
+    # @order = Order.find(params[:id])
+    # @ordere_details = @order.ordere_details
+  end
 
 
   def order_params
-    params.require(:order).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :amount, :purchase_price, :item_id)
+    params.require(:order).permit(:shipping_postal_code, :shipping_address, :shipping_name, :payment_method, :customer_id, :total_amount, :postage )
   end
 end
