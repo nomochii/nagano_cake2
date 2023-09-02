614e7e13adb5f1361dfa06a088230ad0d7e77713
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-09-02 14:48:26 +0000

    [Fix]管理者側会員詳細の苗字と名前の表示順変更・注文履歴一覧リンク追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,10 +12,10 @@ class Customer < ApplicationRecord
   end
 
   def full_name
-    self.last_name + " " + self.first_name
+    self.first_name + " " + self.last_name
   end
 
   def full_name_kana
-    self.last_name_kana + " " + self.first_name_kana
+    self.first_name_kana + " " + self.last_name_kana
   end
 end
